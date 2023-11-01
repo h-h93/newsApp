9389e9ddd64c3855135e90afdfbfc04514185a84
h-93 <hanif234@hotmail.com>
2023-11-01 20:57:05 +0000

    project complete

--- NewsApp/Views/NewsCollectionViewController.swift
+++ NewsApp/Views/NewsCollectionViewController.swift
@@ -10,7 +10,19 @@ import UIKit
 private let reuseIdentifier = "Cell"
 
 class NewsCollectionViewController: UICollectionViewController {
-
+    
+    let newsApiGetNews = NewsAPIGetNews()
+    var articles = [Article]()
+    
+    override init(collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(collectionViewLayout: layout)
+        newsApiGetNews.delegate = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,38 +30,37 @@ class NewsCollectionViewController: UICollectionViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        self.collectionView!.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
         // Do any additional setup after loading the view.
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    
+    func loadArticles(articles: [Article]) {
+        self.articles = articles
+        self.collectionView.reloadData()
     }
-    */
+    
 
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return articles.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CustomCollectionViewCell
+
         // Configure the cell
+        cell.titleLabel.text = articles[indexPath.item].title
+        print(articles[indexPath.item].urlToImage)
     
         return cell
     }
